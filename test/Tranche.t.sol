b238cae0d82a105e5d55cfff29dba564db31bed1
Alexander <brettbotto@gmail.com>
2023-09-18 15:02:37 -0400

    Remove console import

--- test/Tranche.t.sol
+++ test/Tranche.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {console} from "forge-std/console.sol";
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {IGauge} from "velodrome-finance/contracts/interfaces/IGauge.sol";
